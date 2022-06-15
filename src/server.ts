f25a8d4c6b114383b9a1625415fa6ebc07c404ed
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-15 01:00:15 -0300

    refactor: resolve eslint errors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello World!" })
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World!' }));
 
-app.listen(3000, () => console.log("Server is running!"))
+app.listen(3000, () => console.log('Server is running!'));
