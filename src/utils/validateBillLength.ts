e5654d8599eccc7909d81879a3087b00e94da0a9
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-15 20:12:24 -0300

    refactor: removed unused app error from validate bill length

--- src/utils/validateBillLength.ts
+++ src/utils/validateBillLength.ts
@@ -1,8 +1,6 @@
-import { AppError } from '../shared/errors/AppError';
-
 export const validateBillLength = (bill_number: string) => {
   if (bill_number.length !== 47 && bill_number.length !== 48) {
-    throw new AppError('Invalid bill length!');
+    return false;
   }
   return true;
 };
