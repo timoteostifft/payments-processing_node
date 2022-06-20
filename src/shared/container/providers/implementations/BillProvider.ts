bcb3b6a4125fa15ebdf6af28f480723889690c5a
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-20 19:12:07 -0300

    feature: create get bar code method on bill provider

--- src/shared/container/providers/implementations/BillProvider.ts
+++ src/shared/container/providers/implementations/BillProvider.ts
@@ -24,6 +24,17 @@ class BillProvider implements IBillProvider {
     const verifiedDigit = 10 - (sum % 10);
     return verifiedDigit === verifyingDigit;
   }
+
+  getBarCode(digitable_line: string): string {
+    const barCode = ''
+      .concat(digitable_line.slice(0, 4))
+      .concat(digitable_line[32])
+      .concat(digitable_line.slice(33))
+      .concat(digitable_line.slice(4, 9))
+      .concat(digitable_line.slice(10, 20))
+      .concat(digitable_line.slice(21, 31));
+    return barCode;
+  }
 }
 
 export { BillProvider };
