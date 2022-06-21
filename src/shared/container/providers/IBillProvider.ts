35216a3c33b270539c75cc5f03b735b509d50c14
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-21 19:18:13 -0300

    feature: add type parameter to validate bar code method on bill provider interface

--- src/shared/container/providers/IBillProvider.ts
+++ src/shared/container/providers/IBillProvider.ts
@@ -1,7 +1,7 @@
 interface IBillProvider {
   validateField(field: string, verifyingDigit: number): boolean
   getBarCode(digitable_line: string, type: string): string
-  validateBarCode(barCode: string): boolean
+  validateBarCode(barCode: string, type: string): boolean
   getAmount(digitable_line: string): string
   getExpirationDate(digitable_line: string): Date | null
 }
