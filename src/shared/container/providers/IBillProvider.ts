435a5c770b340e393901db181db4ea75e9c11dec
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-20 23:59:22 -0300

    feature: create validate bar code method on bill provider interface

--- src/shared/container/providers/IBillProvider.ts
+++ src/shared/container/providers/IBillProvider.ts
@@ -1,6 +1,7 @@
 interface IBillProvider {
   validateField(field: string, verifyingDigit: number): boolean
   getBarCode(digitable_line: string): string
+  validateBarCode(barCode: string): boolean
 }
 
 export { IBillProvider };
