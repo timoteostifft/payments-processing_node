7a9990963b6f1463a20f866bff18886334bff417
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-21 10:09:33 -0300

    feature: create get expiration date method on bill provider interface

--- src/shared/container/providers/IBillProvider.ts
+++ src/shared/container/providers/IBillProvider.ts
@@ -3,6 +3,7 @@ interface IBillProvider {
   getBarCode(digitable_line: string): string
   validateBarCode(barCode: string): boolean
   getAmount(digitable_line: string): string
+  getExpirationDate(digitable_line: string): Date | null
 }
 
 export { IBillProvider };
