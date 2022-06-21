1a265f13b1df769ffbd1a29677e326b2e37b81f0
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-21 16:51:44 -0300

    feature: add type parameter to get bar code method on bill provider interface, allowing it to check bill type

--- src/shared/container/providers/IBillProvider.ts
+++ src/shared/container/providers/IBillProvider.ts
@@ -1,6 +1,6 @@
 interface IBillProvider {
   validateField(field: string, verifyingDigit: number): boolean
-  getBarCode(digitable_line: string): string
+  getBarCode(digitable_line: string, type: string): string
   validateBarCode(barCode: string): boolean
   getAmount(digitable_line: string): string
   getExpirationDate(digitable_line: string): Date | null
