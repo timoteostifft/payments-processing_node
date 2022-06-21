1ab814378a67cb847b2d19e51f1d495a4a933b94
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-21 01:38:43 -0300

    feature: create get amount method on bill provider interface

--- src/shared/container/providers/IBillProvider.ts
+++ src/shared/container/providers/IBillProvider.ts
@@ -2,6 +2,7 @@ interface IBillProvider {
   validateField(field: string, verifyingDigit: number): boolean
   getBarCode(digitable_line: string): string
   validateBarCode(barCode: string): boolean
+  getAmount(digitable_line: string): string
 }
 
 export { IBillProvider };
