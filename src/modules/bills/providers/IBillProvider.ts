4f80c319366e9516c3e4a15f14cd568a7413d130
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-22 18:45:49 -0300

    feat: add type parameter to get amount method on bill provider to check bill type

--- src/modules/bills/providers/IBillProvider.ts
+++ src/modules/bills/providers/IBillProvider.ts
@@ -2,7 +2,7 @@ interface IBillProvider {
   validateField(field: string, verifyingDigit: number): boolean
   getBarCode(digitable_line: string, type: string): string
   validateBarCode(barCode: string, type: string): boolean
-  getAmount(digitable_line: string): string
+  getAmount(digitable_line: string, type: string): string
   getExpirationDate(digitable_line: string): Date | null
 }
 
