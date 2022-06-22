b5a1a4063189018ae07f1e286107c1866c3d1e70
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-22 18:51:37 -0300

    refactor: move validate bill length to library

--- src/shared/http/middlewares/ensureValidBill.ts
+++ src/shared/http/middlewares/ensureValidBill.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-import { validateDigitableLineLength } from '../../../utils/validateBillLength';
+import { validateDigitableLineLength } from '../../../library/validateBillLength';
 import { AppError } from '../../errors/AppError';
 
 export async function ensureValidBill(request: Request, response: Response, next: NextFunction) {
