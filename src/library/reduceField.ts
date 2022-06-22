a13d2f853a26ed4d93d6afbce338034aaa8c30c0
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-22 19:09:57 -0300

    feat: create reduce field library method

---
+++ src/library/reduceField.ts
@@ -0,0 +1,19 @@
+import { reduceDigit } from '../utils/reduceDigit';
+
+export const reduceField = (field: string[]): number => {
+  const reducedFieldSum = field.reduce((total, value, index) => {
+    let digit = parseInt(value, 10);
+
+    if (index % 2) {
+      digit *= 1;
+    } else {
+      digit *= 2;
+    }
+
+    if (digit > 9) {
+      digit = reduceDigit(digit);
+    }
+    return total + digit;
+  }, 0);
+  return reducedFieldSum;
+};
