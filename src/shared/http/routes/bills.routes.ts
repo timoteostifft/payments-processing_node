e977d9b78286d61b85686eee03c2c6c3d1cea02f
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-22 20:54:37 -0300

    feat: implement ensure valid bill on bill routes

--- src/shared/http/routes/bills.routes.ts
+++ src/shared/http/routes/bills.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { ValidateBillController } from '../../../modules/bills/useCases/validateBill/ValidateBillController';
+import { ensureValidBill } from '../middlewares/ensureValidBill';
 
 const billsRoutes = Router();
 
 const validateBillController = new ValidateBillController();
 
-billsRoutes.get('/:digitable_line', validateBillController.handle);
+billsRoutes.get('/:digitable_line', ensureValidBill, validateBillController.handle);
 
 export { billsRoutes };
