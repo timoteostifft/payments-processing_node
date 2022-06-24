cbfb57b57dc93929b9d046820a76f7a18b5a1a1a
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-24 10:37:22 -0300

    refactor: removed unused console log

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,5 @@
 export const formatDate = (date: Date | null): string | null => {
   if (date) {
-    console.log(date);
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   }
   return null;
